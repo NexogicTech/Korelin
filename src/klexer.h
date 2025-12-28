1884b7221683ac75cbf9b6ffca9f50d983576ddb
NexogicTech <8@nexogic.org>
2025-12-28 10:58:55 +0800

    Add klexer source files

---
+++ src/klexer.h
@@ -0,0 +1,23 @@
+//
+// Created by Helix on 2025/12/28.
+//
+
+#ifndef KORELIN_LEXER_H
+#define KORELIN_LEXER_H
+
+typedef enum {
+    // 特殊 Token
+    KORELIN_EOF,
+    KORELIN_ERROR,
+
+    // 分隔符
+    KORELIN_COMMA,     // ,
+    KORELIN_SEMICOLON, // ;
+    TOKEN_LPAREN, // (
+    TOKEN_RPAREN, // )
+    TOKEN_LBRACE, // {
+    TOKEN_RBRACE, // }
+
+} TokenType;
+
+#endif //KORELIN_LEXER_H
